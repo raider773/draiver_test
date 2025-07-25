e6c120111fd7dab4acdd2aec38801eab5dd36b13
raider773 <gonzalopereyrametnik@outlook.com>
2025-07-25 12:31:58 -0300

    vector database

--- app/inference_engine/answer.py
+++ app/inference_engine/answer.py
@@ -1,10 +1,20 @@
 from app.agents.toolSelectorAgent import ToolSelectorAgent
+from app.utils.utils import get_france_weather, make_reservation
 
 API_KEY = "".strip() 
 tool_agent = ToolSelectorAgent(api_key=API_KEY)
 
-user_query = "What will the weather be in july?"
-response = tool_agent.decide_tool(user_query)
+#user_query = "What will the weather be in july?"
+user_query = "Book me a place in effeil tower in august"
 
 
-print(response)
+#Tool Agent
+tool_information = "No extra information needed"
+tool_response = tool_agent.decide_tool(user_query)   
+
+if tool_response["tool"] == "get_weather":
+    tool_information = get_france_weather(tool_response["parameters"]["date"])
+
+elif tool_response["tool"] == "make_reservation":
+    tool_information = make_reservation(tool_response["parameters"]["activity"],tool_response["parameters"]["date"])
+
---
+++ app/utils/utils.py
@@ -0,0 +1,6 @@
+def get_france_weather(date: str) -> str:
+    return f"In {date}, the weather is expected to be sunny with temperatures around 28–35°C."
+
+
+def make_reservation(activity: str, date: str) -> str:
+    return f"Reservation confirmed for '{activity}' on {date}."
