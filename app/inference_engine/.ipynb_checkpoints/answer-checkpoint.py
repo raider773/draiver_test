3c423891afc1464b76d140f5b14731ff71711889
raider773 <gonzalopereyrametnik@outlook.com>
2025-07-25 13:13:51 -0300

    agent memory

--- app/inference_engine/.ipynb_checkpoints/answer-checkpoint.py
+++ app/inference_engine/.ipynb_checkpoints/answer-checkpoint.py
@@ -1,7 +1,8 @@
+import os
+import pickle
 from app.agents.toolSelectorAgent import ToolSelectorAgent
 from app.agents.questionAnswerAgent import QuestionAnswerAgent
-from app.utils.utils import get_france_weather, make_reservation
-from app.utils.utils import search_similar_docs
+from app.utils.utils import get_france_weather, make_reservation, format_history, search_similar_docs
 
 API_KEY = "".strip() 
 tool_agent = ToolSelectorAgent(api_key=API_KEY)
@@ -10,7 +11,18 @@ qa_agent = QuestionAnswerAgent(api_key=API_KEY)
 #user_query = "What will the weather be in july?"
 #user_query = "Book me a place in effeil tower in august"
 user_query = "Where can I get a coffee in Paris?"
+session_id = 1
 
+#Get user chat
+session_file = f"app/knowledge_base/chat_user_{session_id}.pkl"
+if os.path.exists(session_file):
+    with open(session_file, "rb") as f:
+        history = pickle.load(f)
+else:
+    history = []
+formatted_history = format_history(history) if history else "User has no chat history."
+
+print(formatted_history)
 
 #Tool Agent
 tool_information = "No extra information needed"
@@ -29,5 +41,16 @@ context = "\n\n".join(docs)
 
 
 #QA Agent
-answer = qa_agent.answer_user(query = user_query, tool_output = tool_information, context = context)
+answer = qa_agent.answer_user(query = user_query, tool_output = tool_information, context = context, user_chat_history = formatted_history)
+
+# Save history
+history.append({"role": "user", "content": user_query})
+history.append({"role": "assistant", "content": answer})
+os.makedirs(os.path.dirname(session_file), exist_ok=True)
+with open(session_file, "wb") as f:
+    pickle.dump(history, f)
+
 print(answer)
+
+
+
